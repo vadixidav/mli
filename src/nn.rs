82e4b93d24c57856e6c59941edd66606a488a565
vadix <vadixidav@gmail.com>
2016-02-08 10:15:31 -0500

    fixed collenchyma uses

--- src/nn.rs
+++ src/nn.rs
@@ -1,6 +1,10 @@
 //! This module contains neural network implementations.
 extern crate collenchyma as co;
 extern crate collenchyma_nn as nn;
-use co::prelude::*;
-use nn::*;
+use self::co::backend::{Backend, BackendConfig};
+use self::co::framework::IFramework;
+use self::co::frameworks::{Cuda, Native};
+use self::co::memory::MemoryType;
+use self::co::tensor::SharedTensor;
+use self::nn::*;
 use super::{SISO, Learning};
