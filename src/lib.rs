f697bedbfef77722c43556e5fb957e22ad63ac5b
Geordon Worley <vadixidav@gmail.com>
2016-09-01 04:58:13 -0400

    fix comments and add Debug derive

--- src/lib.rs
+++ src/lib.rs
@@ -3,58 +3,58 @@ extern crate rand;
 pub mod mep;
 pub use mep::*;
 
-///SISO is an algorithm that takes a static amount of inputs and produces a static number of outputs.
+/// SISO is an algorithm that takes a static amount of inputs and produces a static number of outputs.
 pub trait SISO<'a, In, Out> {
-    ///The iterator type returned from compute. Its lifetime is tied to self.
-    type Iter: Iterator<Item=Out> + 'a;
-    ///Compute is provided an input slice that must be the correct number of inputs and produces an
-    ///iterator that provides the static number of outputs.
+    /// The iterator type returned from compute. Its lifetime is tied to self.
+    type Iter: Iterator<Item = Out> + 'a;
+    /// Compute is provided an input slice that must be the correct number of inputs and produces an
+    /// iterator that provides the static number of outputs.
     fn compute(&'a self, inputs: &'a [In]) -> Self::Iter;
 }
 
-///SIVO is an algorithm that takes a static amount of inputs and produces a variable number of outputs.
+/// SIVO is an algorithm that takes a static amount of inputs and produces a variable number of outputs.
 pub trait SIVO<'a, In, Out> {
-    ///The iterator type returned from compute. Its lifetime is tied to self.
-    type Iter: Iterator<Item=Out> + 'a;
-    ///Compute is provided an input slice that must be the correct number of inputs along with the number of outputs.
-    ///It returns an iterator that provides the required number of outputs.
+    /// The iterator type returned from compute. Its lifetime is tied to self.
+    type Iter: Iterator<Item = Out> + 'a;
+    /// Compute is provided an input slice that must be the correct number of inputs along with the number of outputs.
+    /// It returns an iterator that provides the required number of outputs.
     fn compute(&'a self, inputs: &'a [In], n_outputs: usize) -> Self::Iter;
 }
 
-///VISO is an algorithm that takes a variable amount of inputs and produces a static number of outputs.
+/// VISO is an algorithm that takes a variable amount of inputs and produces a static number of outputs.
 pub trait VISO<'a, In, Out> {
-    ///The iterator type returned from compute. Its lifetime is tied to self.
-    type Iter: Iterator<Item=Out> + 'a;
-    ///Compute is provided an input slice that can be any number of inputs and produces an
-    ///iterator that provides the static number of outputs.
+    /// The iterator type returned from compute. Its lifetime is tied to self.
+    type Iter: Iterator<Item = Out> + 'a;
+    /// Compute is provided an input slice that can be any number of inputs and produces an
+    /// iterator that provides the static number of outputs.
     fn compute(&'a self, inputs: &'a [In]) -> Self::Iter;
 }
 
-///VIVO is an algorithm that takes a variable amount of inputs and produces a variable number of outputs.
+/// VIVO is an algorithm that takes a variable amount of inputs and produces a variable number of outputs.
 pub trait VIVO<'a, In, Out> {
-    ///The iterator type returned from compute. Its lifetime is tied to self.
-    type Iter: Iterator<Item=Out> + 'a;
-    ///Compute is provided an input slice that can be any number of inputs along with the number of outputs.
-    ///It returns an iterator that provides the required number of outputs.
+    /// The iterator type returned from compute. Its lifetime is tied to self.
+    type Iter: Iterator<Item = Out> + 'a;
+    /// Compute is provided an input slice that can be any number of inputs along with the number of outputs.
+    /// It returns an iterator that provides the required number of outputs.
     fn compute(&'a self, inputs: &'a [In], n_outputs: usize) -> Self::Iter;
 }
 
-///A learning algorithm is one that can be trained and performs computations.
+/// A learning algorithm is one that can be trained and performs computations.
 pub trait Learning<R, In, Out> {
-    ///Train requires inputs and outputs required by the system and an Rng to introduce randomness.
-    ///Level should scale linearly with the amount of time spent training and level 1 should represent the smallest
-    ///amount of training reasonably possible.
+    /// Train requires inputs and outputs required by the system and an Rng to introduce randomness.
+    /// Level should scale linearly with the amount of time spent training and level 1 should represent the smallest
+    /// amount of training reasonably possible.
     fn train(&mut self, level: u32, inputs: &[In], outputs: &[Out], rng: &mut R);
 }
 
-///Genetic is a trait that allows genetic manipulation. Genetic algorithms require duplication, which is why
-///there is a requirement for Clone. It is parameterized with an Rng (R) type so that the algorithms can extract random
-///data when mating so that mating can happen randomly.
+/// Genetic is a trait that allows genetic manipulation. Genetic algorithms require duplication, which is why
+/// there is a requirement for Clone. It is parameterized with an Rng (R) type so that the algorithms can extract random
+/// data when mating so that mating can happen randomly.
 ///
-///Note: This API is highly likely to change before version 1.0.
-pub trait Genetic<R> : Clone {
-    ///The mate function takes a tuple of two parent references and an Rng, then returns a new child.
+/// Note: This API is highly likely to change before version 1.0.
+pub trait Genetic<R>: Clone {
+    /// The mate function takes a tuple of two parent references and an Rng, then returns a new child.
     fn mate(parents: (&Self, &Self), rng: &mut R) -> Self;
-    ///The mutate function performs a unit mutation. A single, several, or no actual mutations may occour.
+    /// The mutate function performs a unit mutation. A single, several, or no actual mutations may occour.
     fn mutate(&mut self, rng: &mut R);
 }
--- src/mep.rs
+++ src/mep.rs
@@ -7,17 +7,18 @@ use std::iter::Rev;
 use super::{Genetic, SISO};
 use std::marker::PhantomData;
 
-///Defines an opcode for the Mep. Every opcode contains an instruction and two parameter indices. These specify which
-///previous opcodes produced the result required as inputs to this opcode. These parameters can also come from the inputs
-///to the program, which sequentially preceed the internal instructions.
-#[derive(Clone, Copy)]
+/// Defines an opcode for the Mep. Every opcode contains an instruction and two parameter indices. These specify which
+/// previous opcodes produced the result required as inputs to this opcode. These parameters can also come from the inputs
+/// to the program, which sequentially preceed the internal instructions.
+#[derive(Debug, Clone, Copy)]
 struct Opcode<Ins> {
     instruction: Ins,
     first: usize,
     second: usize,
 }
 
-///A multi-expression program represented using a series of operations that can reuse results of previous operations.
+/// A multi-expression program represented using a series of operations that can reuse results of previous operations.
+#[derive(Debug)]
 pub struct Mep<Ins, R, Param, F1, F2> {
     program: Vec<Opcode<Ins>>,
     pub unit_mutate_size: usize,
@@ -30,10 +31,12 @@ pub struct Mep<Ins, R, Param, F1, F2> {
 }
 
 impl<Ins, R, Param, F1, F2> Clone for Mep<Ins, R, Param, F1, F2>
-    where Ins: Clone, F1: Copy, F2: Copy
+    where Ins: Clone,
+          F1: Copy,
+          F2: Copy
 {
     fn clone(&self) -> Self {
-        Mep{
+        Mep {
             program: self.program.clone(),
             unit_mutate_size: self.unit_mutate_size,
             crossover_points: self.crossover_points,
@@ -47,15 +50,25 @@ impl<Ins, R, Param, F1, F2> Clone for Mep<Ins, R, Param, F1, F2>
 }
 
 impl<Ins, R, Param, F1, F2> Mep<Ins, R, Param, F1, F2> {
-    ///Generates a new Mep with a particular size and takes a closure to generate random instructions.
-    ///Takes an RNG as well to generate random internal data for each instruction. This Rng is different from the Mep's
-    ///Rng, thus it is parameterized separately here.
-    pub fn new<I, Rg>(inputs: usize, outputs: usize, unit_mutate_size: usize, crossover_points: usize, rng: &mut Rg,
-        instruction_iter: I, mutator: F1, processor: F2) -> Self
-        where I: ExactSizeIterator<Item=Ins>, Rg: Rng, F1: Fn(&mut Ins, &mut R), F2: Fn(&Ins, Param, Param) -> Param
+    /// Generates a new Mep with a particular size and takes a closure to generate random instructions.
+    /// Takes an RNG as well to generate random internal data for each instruction. This Rng is different from the Mep's
+    /// Rng, thus it is parameterized separately here.
+    pub fn new<I, Rg>(inputs: usize,
+                      outputs: usize,
+                      unit_mutate_size: usize,
+                      crossover_points: usize,
+                      rng: &mut Rg,
+                      instruction_iter: I,
+                      mutator: F1,
+                      processor: F2)
+                      -> Self
+        where I: ExactSizeIterator<Item = Ins>,
+              Rg: Rng,
+              F1: Fn(&mut Ins, &mut R),
+              F2: Fn(&Ins, Param, Param) -> Param
     {
         let outputs_beginning = instruction_iter.len() - outputs;
-        Mep{
+        Mep {
             program: instruction_iter.enumerate()
                 .map(|(index, ins)|
                     // For normal instructions previous outputs may be used.
@@ -72,8 +85,7 @@ impl<Ins, R, Param, F1, F2> Mep<Ins, R, Param, F1, F2> {
                             first: rng.gen_range(0, outputs_beginning + inputs),
                             second: rng.gen_range(0, outputs_beginning + inputs),
                         }
-                    }
-                )
+                    })
                 .collect(),
             unit_mutate_size: unit_mutate_size,
             crossover_points: crossover_points,
@@ -87,14 +99,18 @@ impl<Ins, R, Param, F1, F2> Mep<Ins, R, Param, F1, F2> {
 }
 
 impl<Ins, R, Param, F1, F2> Genetic<R> for Mep<Ins, R, Param, F1, F2>
-    where R: Rng, Ins: Clone, Param: Clone, F1: Copy + Fn(&mut Ins, &mut R), F2: Copy
+    where R: Rng,
+          Ins: Clone,
+          Param: Clone,
+          F1: Copy + Fn(&mut Ins, &mut R),
+          F2: Copy
 {
     fn mate(parents: (&Self, &Self), rng: &mut R) -> Self {
-        //Each Mep must have the same amount of inputs
-        //TODO: Once Rust implements generic values, this can be made explicit and is not needed
+        // Each Mep must have the same amount of inputs
+        // TODO: Once Rust implements generic values, this can be made explicit and is not needed
         assert!(parents.0.inputs == parents.1.inputs);
         assert!(parents.0.outputs == parents.1.outputs);
-        //Get the smallest of the two lengths
+        // Get the smallest of the two lengths
         let total_instructions = cmp::min(parents.0.program.len(), parents.1.program.len());
         let crossover_choice = rng.gen_range(0, 2);
         Mep{program:
@@ -141,45 +157,53 @@ impl<Ins, R, Param, F1, F2> Genetic<R> for Mep<Ins, R, Param, F1, F2>
         }
     }
 
-    ///The Mep mutate function operates using the unit_mutate_size. This variable specifies the amount of instructions
-    ///for which a single mutation is expect to occour every time mutate is called. This variable can be mutated inside
-    ///of mutate, in which case it may never go below 1, but may tend towards infinity in increments of 1. This variable
-    ///is implemented as a u64 to permit it to expand unbounded to mutation levels that are so low that mutations
-    ///virtually never happen. Allowing this to mutate allows species to find the equilibrium between genomic
-    ///adaptability and stability. If a species develops information gathering, then it can adapt intellegently, making
-    ///it possibly more beneficial to operate at lower mutation rates. Setting the default mutation rate for species
-    ///properly, or allowing it to adapt as the simulation continues, permits species to survive more frequently that
-    ///are randomly generated.
+    /// The Mep mutate function operates using the unit_mutate_size. This variable specifies the amount of instructions
+    /// for which a single mutation is expect to occour every time mutate is called. This variable can be mutated inside
+    /// of mutate, in which case it may never go below 1, but may tend towards infinity in increments of 1. This variable
+    /// is implemented as a u64 to permit it to expand unbounded to mutation levels that are so low that mutations
+    /// virtually never happen. Allowing this to mutate allows species to find the equilibrium between genomic
+    /// adaptability and stability. If a species develops information gathering, then it can adapt intellegently, making
+    /// it possibly more beneficial to operate at lower mutation rates. Setting the default mutation rate for species
+    /// properly, or allowing it to adapt as the simulation continues, permits species to survive more frequently that
+    /// are randomly generated.
     fn mutate(&mut self, rng: &mut R) {
-        //Mutate unit_mutate_size
+        // Mutate unit_mutate_size
         if rng.gen_range(0, self.unit_mutate_size) == 0 {
-            //Make it possibly go up or down by 1
+            // Make it possibly go up or down by 1
             match rng.gen_range(0, 2) {
                 0 => self.unit_mutate_size += 1,
-                1 => if self.unit_mutate_size > 1 {self.unit_mutate_size -= 1},
+                1 => {
+                    if self.unit_mutate_size > 1 {
+                        self.unit_mutate_size -= 1
+                    }
+                }
                 _ => unreachable!(),
             }
         }
-        //Mutate crossover_points
+        // Mutate crossover_points
         if rng.gen_range(0, self.unit_mutate_size) == 0 {
-            //Make it possibly go up or down by 1
+            // Make it possibly go up or down by 1
             match rng.gen_range(0, 2) {
                 0 => self.crossover_points += 1,
-                1 => if self.crossover_points > 1 {self.crossover_points -= 1},
+                1 => {
+                    if self.crossover_points > 1 {
+                        self.crossover_points -= 1
+                    }
+                }
                 _ => unreachable!(),
             }
         }
 
-        //Mutate the instructions using the mutator
+        // Mutate the instructions using the mutator
         loop {
-            //Choose a random location in the instructions and then add a random value up to the unit_mutate_size
+            // Choose a random location in the instructions and then add a random value up to the unit_mutate_size
             let choice = rng.gen_range(0, self.program.len() + self.unit_mutate_size);
-            //Whenever we choose a location outside the vector reject the choice and end mutation
+            // Whenever we choose a location outside the vector reject the choice and end mutation
             if choice >= self.program.len() {
                 break;
             }
             let op = &mut self.program[choice];
-            //Randomly mutate only one of the things contained here
+            // Randomly mutate only one of the things contained here
             match rng.gen_range(0, 3) {
                 0 => (self.mutator)(&mut op.instruction, rng),
                 1 => op.first = rng.gen_range(0, choice + self.inputs),
@@ -190,18 +214,25 @@ impl<Ins, R, Param, F1, F2> Genetic<R> for Mep<Ins, R, Param, F1, F2>
     }
 }
 
-impl<'a, Ins: 'a, R: 'a, Param: 'a, F1: 'a, F2: 'a> SISO<'a, Param, Param> for Mep<Ins, R, Param, F1, F2>
-    where F2: Fn(&Ins, Param, Param) -> Param, Param: Clone
+impl<'a, Ins: 'a, R: 'a, Param: 'a, F1: 'a, F2: 'a> SISO<'a, Param, Param> for Mep<Ins,
+                                                                                   R,
+                                                                                   Param,
+                                                                                   F1,
+                                                                                   F2>
+    where F2: Fn(&Ins, Param, Param) -> Param,
+          Param: Clone
 {
     type Iter = ResultIterator<'a, Ins, R, Param, F1, F2>;
 
     fn compute(&'a self, inputs: &'a [Param]) -> Self::Iter {
-        //Ensure we have enough opcodes to produce the desired amount of outputs, otherwise the programmer has failed
+        // Ensure we have enough opcodes to produce the desired amount of outputs, otherwise the programmer has failed
         assert!(self.outputs <= self.program.len());
-        ResultIterator{
+        ResultIterator {
             mep: self,
             buff: vec![None; self.program.len()],
-            solve_iter: ((self.program.len() + self.inputs - self.outputs)..(self.program.len() + self.inputs)).rev(),
+            solve_iter: ((self.program.len() + self.inputs - self.outputs)..(self.program.len() +
+                                                                             self.inputs))
+                .rev(),
             inputs: inputs,
         }
     }
@@ -215,35 +246,30 @@ pub struct ResultIterator<'a, Ins: 'a, R: 'a, Param: 'a, F1: 'a, F2: 'a> {
 }
 
 impl<'a, Ins, R, Param, F1, F2> ResultIterator<'a, Ins, R, Param, F1, F2>
-    where F2: Fn(&Ins, Param, Param) -> Param, Param: Clone
+    where F2: Fn(&Ins, Param, Param) -> Param,
+          Param: Clone
 {
     fn op_solved(&mut self, i: usize) -> Param {
-        //If this is an input, it is already solved, so return the result immediately
+        // If this is an input, it is already solved, so return the result immediately
         if i < self.mep.inputs {
-            return unsafe{
-                self.inputs.get_unchecked(i)
-            }.clone();
+            return unsafe { self.inputs.get_unchecked(i) }.clone();
         }
-        //Check if this has been evaluated or not
-        let possible = unsafe{
-            self.buff.get_unchecked(i - self.mep.inputs)
-        }.clone();
+        // Check if this has been evaluated or not
+        let possible = unsafe { self.buff.get_unchecked(i - self.mep.inputs) }.clone();
         match possible {
-            //If it has, return the value
+            // If it has, return the value
             Some(x) => x,
-            //If it hasnt been solved
+            // If it hasnt been solved
             None => {
-                //Get a reference to the opcode
-                let op = unsafe{
-                    self.mep.program.get_unchecked(i - self.mep.inputs)
-                };
-                //Compute the result of the operation, ensuring the inputs are solved beforehand
-                let result = (self.mep.processor)(&op.instruction, self.op_solved(op.first), self.op_solved(op.second));
-                //Properly store the Some result to the buffer
-                unsafe{
-                    *self.buff.get_unchecked_mut(i - self.mep.inputs) = Some(result.clone())
-                };
-                //Return the result
+                // Get a reference to the opcode
+                let op = unsafe { self.mep.program.get_unchecked(i - self.mep.inputs) };
+                // Compute the result of the operation, ensuring the inputs are solved beforehand
+                let result = (self.mep.processor)(&op.instruction,
+                                                  self.op_solved(op.first),
+                                                  self.op_solved(op.second));
+                // Properly store the Some result to the buffer
+                unsafe { *self.buff.get_unchecked_mut(i - self.mep.inputs) = Some(result.clone()) };
+                // Return the result
                 result
             }
         }
@@ -251,7 +277,8 @@ impl<'a, Ins, R, Param, F1, F2> ResultIterator<'a, Ins, R, Param, F1, F2>
 }
 
 impl<'a, Ins, R, Param, F1, F2> Iterator for ResultIterator<'a, Ins, R, Param, F1, F2>
-    where F2: Fn(&Ins, Param, Param) -> Param, Param: Clone
+    where F2: Fn(&Ins, Param, Param) -> Param,
+          Param: Clone
 {
     type Item = Param;
     fn next(&mut self) -> Option<Param> {
@@ -269,13 +296,23 @@ mod tests {
     use super::super::{Genetic, SISO};
 
     fn mutator(_: &mut i32, _: &mut Isaac64Rng) {}
-    fn processor(_: &i32, _: i32, _: i32) -> i32 {0}
+    fn processor(_: &i32, _: i32, _: i32) -> i32 {
+        0
+    }
 
     #[test]
     fn new() {
-        let a = Mep::new(3, 1, 10, 10, &mut Isaac64Rng::from_seed(&[1, 2, 3, 4]), 0..30, mutator, processor);
+        let a = Mep::new(3,
+                         1,
+                         10,
+                         10,
+                         &mut Isaac64Rng::from_seed(&[1, 2, 3, 4]),
+                         0..30,
+                         mutator,
+                         processor);
 
-        assert_eq!(a.program.iter().map(|i| i.instruction).collect::<Vec<_>>(), (0..30).collect::<Vec<_>>());
+        assert_eq!(a.program.iter().map(|i| i.instruction).collect::<Vec<_>>(),
+                   (0..30).collect::<Vec<_>>());
     }
 
     #[test]
@@ -287,7 +324,7 @@ mod tests {
         };
         let old_rngs: Vec<_> = rng.clone().gen_iter::<i32>().take(5).collect();
         let _ = Mep::mate((&a, &b), &mut rng);
-        //Ensure that rng was borrowed mutably
+        // Ensure that rng was borrowed mutably
         assert!(rng.clone().gen_iter::<i32>().take(5).collect::<Vec<_>>() != old_rngs);
     }
 
