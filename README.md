e36fda26df4b236d0e1ccf2f3c257fa3aba3e8d2
Geordon Worley <vadixidav@gmail.com>
2018-09-18 00:17:29 -0400

    add new manifesto for the project to the docs and readme

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # mli
-Machine Learning Interface for Rust
 
-[Documentation](https://docs.rs/mli/0.9.0/mli/)
+MLI intends to provide modern, data-driven abstractions for machine learning.
 
-Implementing libraries:
-- [mli_mep](https://github.com/vadixidav/mli_mep)
+MLI provides traits that work much like Combine's `Parser`,
+Serde's `Serialize` and `Deserialize`, std's `Iterator`, and future's `Future` and `Stream`.
+One should only need to have to create tensor processing primitives and then string them together to craft
+a large system that continues to be useful as a primitive in an even larger system.
 
-This library presently has separate generic traits for learning and genetic algorithms.
+## Goals
+
+- Very fast CPU forwards and backwards propogation.
+- Abstraction over multiple backends.
+- Automatic serialization/deserialization  derivations of all state data (using serde).
+- Get complete reuse out of tensor code without any overhead for abstractions.
+- Allow building tensor pipelines that can talk over streams to allow multi-node setups.
+  - It is a non-goal to implement the orchestration of these pipelines in this crate.
+
+## Const Generics
+
+Const generics must land before this API can be stabilized. This API will not be able to interact
+with tensor sizes and dimensions to allow static checking without const generics. Until that lands in stable
+and nalgebra is updated to use it, this library will continue to be in an unstable state.
--- src/lib.rs
+++ src/lib.rs
@@ -1,92 +1,21 @@
+//! MLI intends to provide modern, data-driven abstractions for machine learning.
+//!
+//! MLI provides traits that work much like Combine's `Parser`,
+//! Serde's `Serialize` and `Deserialize`, std's `Iterator`, and future's `Future` and `Stream`.
+//! One should only need to have to create tensor processing primitives and then string them together to craft
+//! a large system that continues to be useful as a primitive in an even larger system.
+//!
+//! ## Goals
+//! - Very fast CPU forwards and backwards propogation.
+//! - Abstraction over multiple backends.
+//! - Automatic serialization/deserialization  derivations of all state data (using serde).
+//! - Get complete reuse out of tensor code without any overhead for abstractions.
+//! - Allow building tensor pipelines that can talk over streams to allow multi-node setups.
+//!   - It is a non-goal to implement the orchestration of these pipelines in this crate.
+//!
+//! ## Const Generics
+//! Const generics must land before this API can be stabilized. This API will not be able to interact
+//! with tensor sizes and dimensions to allow static checking without const generics. Until that lands in stable
+//! and nalgebra is updated to use it, this library will continue to be in an unstable state.
+
 #![no_std]
-
-/// Interface for algorithms which implement the capability to perform stateless computation.
-pub trait Stateless<'a, I, O> {
-    fn process(&'a self, input: I) -> O;
-}
-
-/// Interface for algorithms which implement the capability to perform stateful computation.
-pub trait Stateful<'a, I, O> {
-    fn process(&'a mut self, input: I) -> O;
-}
-
-/// Interface for algorithms which are consumed when computing.
-pub trait Instruction<I, O> {
-    fn process(self, input: I) -> O;
-}
-
-/// Any stateless algorithm can also be used in the same context as a stateful algorithm.
-impl<'a, I, O> Stateful<'a, I, O> for Stateless<'a, I, O> {
-    fn process(&'a mut self, input: I) -> O {
-        Stateless::process(self, input)
-    }
-}
-
-/// Implement this on algorithms which can be trained online.
-///
-/// This assumes that the algorithm stores internal state regarding previous training information.
-pub trait Online<S> {
-    fn train_online(&mut self, set: S);
-}
-
-/// Like `Online`, but also requires an Rng.
-pub trait OnlineRand<S, R> {
-    fn train_online_rng(&mut self, set: S, rng: &mut R);
-}
-
-impl<S, R, O> OnlineRand<S, R> for O
-    where O: Online<S>
-{
-    fn train_online_rng(&mut self, set: S, _: &mut R) {
-        O::train_online(self, set)
-    }
-}
-
-/// Implement this on algorithms which can be trained offline.
-///
-/// Notice that this also creates the algorithm, rather than updating it. For training algorithms
-/// after they have been created, use `Online`.
-pub trait Offline<S> {
-    fn train_offline(set: S) -> Self;
-}
-
-/// Like `Offline`, but also requires an Rng.
-pub trait OfflineRand<S, R> {
-    fn train_offline_rng(set: S, rng: &mut R) -> Self;
-}
-
-impl<S, R, O> OfflineRand<S, R> for O
-where O: Offline<S>
-{
-    fn train_offline_rng(set: S, _: &mut R) -> Self {
-        O::train_offline(set)
-    }
-}
-
-/// Things which can be mated.
-pub trait Mate {
-    /// The mate function takes two parent references and returns a new child.
-    fn mate(&self, rhs: &Self) -> Self;
-}
-
-/// Things which can be mated using a source of randomness.
-pub trait MateRand<R> {
-    /// The mate function takes two parent references and an Rng (`r`), then returns a new child.
-    /// So long as `r` is deterministic, the output is also deterministic as no other sources of
-    /// randomness are involved.
-    fn mate(&self, rhs: &Self, rng: &mut R) -> Self;
-}
-
-impl<R, M> MateRand<R> for M
-    where M: Mate
-{
-    fn mate(&self, rhs: &Self, _: &mut R) -> Self {
-        M::mate(self, rhs)
-    }
-}
-
-/// Things which can be mutated.
-pub trait Mutate<R> {
-    /// Perform a unit mutation.
-    fn mutate(&mut self, rng: &mut R);
-}
