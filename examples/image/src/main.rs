10b04d93e5dce0dce2573836ed91949b3712a6cc
Geordon Worley <vadixidav@gmail.com>
2019-08-17 23:12:51 -0400

    add convolution and create an example doing a sobel filter

--- examples/image/src/main.rs
+++ examples/image/src/main.rs
@@ -1,2 +1,39 @@
+use image::open;
+use mli::Forward;
+use mli_conv::Conv2;
+use ndarray::{array, Array};
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "image", about = "Loads and convolves an image as a test")]
+struct Opt {
+    /// File to load
+    #[structopt(parse(from_os_str))]
+    file: PathBuf,
+    /// Output location
+    #[structopt(parse(from_os_str))]
+    output: PathBuf,
+}
+
 fn main() {
+    let opt = Opt::from_args();
+    let image = open(opt.file).expect("unable to open input image");
+    let image = image.to_luma();
+    let (width, height) = image.dimensions();
+    let dims = (height as usize, width as usize);
+    let input_image = Array::from_shape_vec(dims, image.into_raw()).unwrap();
+    let input_image = input_image.map(|&n| n as f32);
+    let filter = array![[-2.0, -2.0, 0.0], [-2.0, 0.0, 2.0], [0.0, 2.0, 2.0],];
+    let conv = Conv2(filter);
+    let output_image = conv.forward(input_image);
+    let output_image = output_image.map(|&n| num::clamp(n, 0.0, 255.0) as u8);
+    if let &[height, width] = output_image.shape() {
+        let vec = output_image.into_raw_vec();
+        let image = image::GrayImage::from_raw(width as u32, height as u32, vec)
+            .expect("failed to create image from raw vec");
+        image.save(opt.output).expect("failed to save file");
+    } else {
+        panic!("the output image had more than 2 dimensions");
+    }
 }
--- mli-conv/src/lib.rs
+++ mli-conv/src/lib.rs
@@ -1,7 +1,27 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use mli::*;
+pub use ndarray::{Array, Array2};
+
+pub struct Conv2(pub Array2<f32>);
+
+impl Forward for Conv2 {
+    type Input = Array2<f32>;
+    type Output = Array2<f32>;
+
+    fn forward(&self, input: Self::Input) -> Self::Output {
+        let Self(filter) = self;
+        let filter_dims = filter.raw_dim();
+        let output_dims = (
+            input.shape()[0] + 1 - filter_dims[0],
+            input.shape()[1] + 1 - filter_dims[1],
+        );
+        Array::from_shape_vec(
+            output_dims,
+            input
+                .windows(filter_dims)
+                .into_iter()
+                .map(|view| (view.to_owned() * filter).sum())
+                .collect(),
+        )
+        .expect("convolution produced incorrectly sized output")
     }
 }
