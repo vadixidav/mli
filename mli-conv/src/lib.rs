388e12773aa593f88b7eb9a80d638be180e7a69e
Geordon Worley <vadixidav@gmail.com>
2019-08-20 01:02:24 -0400

    update mli with significantly better direction

--- mli-conv/src/lib.rs
+++ mli-conv/src/lib.rs
@@ -1,13 +1,12 @@
 use mli::*;
-pub use ndarray::{Array, Array2};
+pub use ndarray::{Array, Array2, ArrayView2};
 
 pub struct Conv2(pub Array2<f32>);
 
-impl Forward for Conv2 {
-    type Input = Array2<f32>;
+impl<'a> Forward<ArrayView2<'a, f32>> for Conv2 {
     type Output = Array2<f32>;
 
-    fn forward(&self, input: Self::Input) -> Self::Output {
+    fn forward(&self, input: ArrayView2<'a, f32>) -> Self::Output {
         let Self(filter) = self;
         let filter_dims = filter.raw_dim();
         let output_dims = (
@@ -24,4 +23,15 @@ impl Forward for Conv2 {
         )
         .expect("convolution produced incorrectly sized output")
     }
+}
+
+impl Backward<Input> for Conv2 {
+    type InputDerivative = Array2<f32>;
+    type InternalDerivative = Array2<f32>;
+
+    fn partials(&self, input: Self::Input) -> (Self::InputDerivative, Self::InternalDerivative) {
+        // The internal derivative is
+    }
+
+    fn train(&mut self, _: Self::InternalDerivative, _: Self::Output, _: f32) {}
 }
--- mli/src/lib.rs
+++ mli/src/lib.rs
@@ -1,24 +1,85 @@
 //! MLI intends to provide modern, data-driven abstractions for machine learning.
 //!
-//! MLI provides traits that work much like Combine's `Parser`,
-//! Serde's `Serialize` and `Deserialize`, std's `Iterator`, and future's `Future` and `Stream`.
-//! One should only need to have to create tensor processing primitives and then string them together to craft
+//! MLI provides traits that work much like Combine's `Parser` in that one should only
+//! need to have to create tensor processing primitives and then string them together to craft
 //! a large system that continues to be useful as a primitive in an even larger system.
 //!
-//! ## Goals
-//! - Very fast CPU forwards and backwards propogation.
-//! - Abstraction over multiple backends.
-//! - Automatic serialization/deserialization  derivations of all state data (using serde).
-//! - Get complete reuse out of tensor code without any overhead for abstractions.
-//! - Allow building tensor pipelines that can talk over streams to allow multi-node setups.
-//!   - It is a non-goal to implement the orchestration of these pipelines in this crate.
+//! To understand MLI, we actually just need to go over some basic Rust concepts.
 //!
-//! ## Const Generics
-//! Const generics must land before this API can be stabilized. This API will not be able to interact
-//! with tensor sizes and dimensions to allow static checking without const generics. Until that lands in stable
-//! and nalgebra is updated to use it, this library will continue to be in an unstable state.
+//! In Rust, the following relationships relate the owned versions of data with the borrowed versions:
+//! - `Vec<T>` -> `&mut [T]` -> `&[T]`
+//! - `String` -> `&mut str` -> `&str`
+//! - `PathBuf` -> `Path`
+//! - `OsString` -> `OsStr`
+//! - `FnOnce` -> `FnMut` -> `Fn`
+//!
+//! Most people don't think about the last one, because it is a trait, but it is critical to understand.
+//! A `FnOnce` owns its enclosed environment. It can implement `FnMut` because since it owns the environment,
+//! it can create a `&mut` to it. Similarly, a `FnMut` has a mutable borrow to its environment, thus it can
+//! downgrade that mutable borrow to an immutable borrow.
+//!
+//! In MLI, the equivalent is:
+//! - `Graph` -> `RunGraph`
+//!
+//! Let us decompose the notion of what a compute graph is. A compute graph simply takes several inputs,
+//! produces several intermediary outputs, and then produces the actual outputs. Each one of these
+//! intermediary outputs is important because they affect the gradients of the functions that came after them.
+//! If we discard them, then they will have to be recomputed from the input. If we only have a simple
+//! convolution operation as our whole graph, there are no intermediary computations, just the
+//! input and the output. However, we are more likely to have several layers of convolution, activation
+//! functions, splitting, and merging. If we have just two layers of convolution with no activation function,
+//! the first layer produces an output which is necessary to calculate the gradient `𝛿output/𝛿filter`.
+//!
+//! We would like to have an abstraction that treats a whole graph, its inputs, intermediary computations, and
+//! its outputs, the same way we would treat a single simple static function like a `tanh`. We would also like
+//! that abstraction to be zero-cost and ideally parallel.
+//!
+//! This is where the magic happens. The traits [`Forward`] and [`Backward`] only require a `&self` as an
+//! argument. This is because they use the trainable variables immutably to do a forward and backward
+//! propogation. The trainable variables within the graph are stored as the [`Backward::TrainDelta`] type.
+//! This means that if we have a graph composed of [`Forward`] and [`Backward`] items, we can compute
+//! all of the deltas for a batch without ever mutating the graph, thus we can compute all of the deltas
+//! **in parallel** for each batch. Since the learning rate is intended to be incorporated into the
+//! delta before hand, you can just sum all of the deltas in the batch and train the network.
+//!
+//! The network which is trained though can't possibly be the one we distributed to all the threads,
+//! however, due to Rust's ownership and borrowing. This is where the `Graph` and the `RunGraph` come in.
+//! The `Graph` is the trainable graph which we have complete ownership over. The `RunGraph` immutably borrows
+//! the `Graph`, so we can create a lot of `RunGraph` and propogate [`Forward`] and [`Backward`] across them
+//! in multiple threads. We can then sum all of the deltas from the threads when they are done and then
+//! update the `Graph`, which is no longer borrowed.
+//!
+//! Currently there are four main traits:
+//! - [`Forward`]
+//!     - Implemented on anything that can go into a forward graph.
+//!     - Outputs intermediary computations (to compute gradients later) and final output.
+//! - [`Backward`]
+//!     - Implemented on anything that can go into a backward graph.
+//!     - Propogates gradients backwards through the neural network.
+//!     - This is provided the original input and any intermediary computations.
+//!     - Propogates the change from the output to the input and trainable variables.
+//!     - Is `&self` and does not do the actual training, so this can be ran in parallel.
+//! - [`Train`]
+//!     - Implemented on anything that can go into a training graph.
+//!     - Uses the change to update the trainable variables.
+//!         - Change can be normalized across a mini-batch before being passed.
+//!     - Implemented on the `mutable` version of the graph.
+//! - [`Static`]
+//!     - Implemented on any operation (or graph) which has no trainable variables inside.
+//!     - Just needs to propogate the gradients from
 
 #![no_std]
+
+/// This trait is for algorithms that have an input and produce an output.
+pub trait Forward<Input> {
+    type Output;
+    type Internal;
+
+    /// `forward` produces:
+    /// - All intermediary values produced, which can be reused in the back propogation step
+    /// - The output `f` given an `input`
+    fn forward(&self, input: Input) -> (Self::Internal, Self::Output);
+}
 
 /// This trait indicates support of backwards propogation.
 ///
@@ -26,70 +87,124 @@
 /// If training is not possible, this trait can still be implemented with those definitions
 /// being empty. In that case, machine learning algorithms will still be able to back propogate
 /// over this operation, but training it will be a no-op.
-pub trait Backward: Forward {
-    type InputDerivative;
-    type InternalDerivative;
+pub trait Backward<Input, OutputDelta>: Forward<Input> {
+    type InputDelta;
+    type TrainDelta;
 
-    /// `partials` produces the partial derivatives `df/dx` and `df/dv` where:
+    /// `partials` produces the change required in the input and trainable variables.
     ///
+    /// Key:
     /// - `f` is the output
     /// - `x` is the input
-    /// - `v` is the internal variables
+    /// - `v` is the trainable variables
+    /// - `E` is the loss or error
+    /// - `delta` is equivalent to `Δf` or `-η * 𝛿E/𝛿f`
     ///
-    /// Either of these can be an approximation, particularly if the function is not differentiable.
-    fn partials(&self, input: Self::Input) -> (Self::InputDerivative, Self::InternalDerivative);
+    /// This method should produce `(Δx, Δv)`:
+    /// - `Δx = Δf * 𝛿f/𝛿x`
+    /// - `Δv = Δf * 𝛿f/𝛿v`
+    ///
+    /// `𝛿f/𝛿x` and `𝛿f/𝛿v` can be an approximation, particularly if the function is not differentiable
+    /// (e.g. ReLU and signum).
+    fn backwards(
+        &self,
+        input: Input,
+        internal: Self::Internal,
+        output_delta: OutputDelta,
+    ) -> (Self::InputDelta, Self::TrainDelta);
 
-    /// `train` takes in `dfdv`, `dedf`, and `rate` where:
-    ///
-    /// - `dfdv` is the partial derivative `df/dv`
-    /// - `dedf` is the partial derivative `dE/df`
-    /// - `f` is the output
-    /// - `E` is the error
-    /// - `v` is the internal variables
-    fn train(&mut self, dfdv: Self::InternalDerivative, dedf: Self::Output, rate: f32);
-
-    /// `partial_input` produces the partial derivative `df/dx`.
-    ///
-    /// See `partials` for more information.
-    fn partial_input(&self, input: Self::Input) -> Self::InputDerivative {
-        self.partials(input).0
+    /// See [`Backward::partials`] for documentation.
+    fn backwards_input(
+        &self,
+        input: Input,
+        internal: Self::Internal,
+        output_delta: OutputDelta,
+    ) -> Self::InputDelta {
+        self.backwards(input, internal, output_delta).0
     }
 
-    /// `partial_internal` produces the partial derivative `df/dv`.
-    ///
-    /// See `partials` for more information.
-    fn partial_internal(&self, input: Self::Input) -> Self::InternalDerivative {
-        self.partials(input).1
+    /// See [`Backward::partials`] for documentation.
+    fn backwards_train(
+        &self,
+        input: Input,
+        internal: Self::Internal,
+        output_delta: OutputDelta,
+    ) -> Self::TrainDelta {
+        self.backwards(input, internal, output_delta).1
     }
 }
 
-pub trait Static: Forward {
-    type Derivative;
+/// This trait is implemented on all operations that can be included in a trainable model.
+/// If your operation is static and has no trainable variables, please implement the [`Static`]
+/// trait instead of this trait directly and this will be implemented as a no-op (no overhead)
+/// automatically through blanket implementation.
+///
+/// This trait is implemented on all structures that implement `Backward` that are mutable
+/// or `Static` even if they are immutable. This exists so that an immutable model can have
+/// back propogation ran against in several different threads without the need for locking,
+/// and then the mutable version of the model can implement `Train` to recieve the gradients
+/// and be updated. If training is not possible, this trait can still be implemented with those
+/// definitions being empty. In that case, machine learning algorithms will still be able to
+/// incorporate this operation, but training it will be a no-op. This is necessary to
+/// implement to be included in a trainable model.
+pub trait Train<Input, OutputDelta>: Backward<Input, OutputDelta> {
+    /// `train` takes in a train delta `Δv` and applies it to the trained variables.
+    ///
+    /// This should effectively perform `v += Δv`.
+    fn train(&mut self, train_delta: Self::TrainDelta);
 
-    /// `partial` produces the partial derivative `df/dx`
-    /// where `f` is the output and `x` in the `input`.
-    fn partial(&self, input: Self::Input) -> Self::Derivative;
+    /// `propogate` should have the same effect as running `backwards` followed by `train`.
+    ///
+    /// See the source (by clicking the SRC link on the right) to see the simple definition.
+    /// If the training is running on the same instance as the backpropogation, then `propogate`
+    /// might perform better since it may not need to allocate more memory to update the trained variables.
+    /// If an implementation can do this efficiently, it should create a custom implemenatation.
+    fn propogate(
+        &mut self,
+        input: Input,
+        internal: Self::Internal,
+        output_delta: OutputDelta,
+    ) -> Self::InputDelta {
+        let (input_delta, train_delta) = self.backwards(input, internal, output_delta);
+        self.train(train_delta);
+        input_delta
+    }
 }
 
-impl<T> Backward for T
+pub trait Static<Input, OutputDelta>: Forward<Input> {
+    type InputDelta;
+    /// `static_propogate` produces the change required in the input.
+    ///
+    /// See the docs for [`Backward::propagate`] to see how to implement this.
+    /// You only need to return `Δx` for this propogate since there are no trainable variables.
+    fn static_propogate(
+        &self,
+        input: Input,
+        internal: Self::Internal,
+        output_delta: OutputDelta,
+    ) -> Self::InputDelta;
+}
+
+impl<T, Input, OutputDelta> Backward<Input, OutputDelta> for T
 where
-    T: Static,
+    T: Static<Input, OutputDelta>,
 {
-    type InputDerivative = T::Derivative;
-    type InternalDerivative = ();
+    type InputDelta = <Self as Static<Input, OutputDelta>>::InputDelta;
+    type TrainDelta = ();
 
-    fn partials(&self, input: Self::Input) -> (Self::InputDerivative, Self::InternalDerivative) {
-        (self.partial(input), ())
+    fn backwards(
+        &self,
+        input: Input,
+        internal: Self::Internal,
+        output_delta: OutputDelta,
+    ) -> (Self::InputDelta, Self::TrainDelta) {
+        (self.static_propogate(input, internal, output_delta), ())
     }
-
-    fn train(&mut self, _: Self::InternalDerivative, _: Self::Output, _: f32) {}
 }
 
-/// This trait is for algorithms that have an input and produce an output.
-pub trait Forward {
-    type Input;
-    type Output;
-
-    /// `forward` produces the output `f` given an `input`.
-    fn forward(&self, input: Self::Input) -> Self::Output;
+impl<T, Input, OutputDelta> Train<Input, OutputDelta> for T
+where
+    T: Static<Input, OutputDelta>,
+{
+    fn train(&mut self, _: Self::TrainDelta) {}
 }
