5397bc2350fd0af21deb3373890cb2909720ded4
Geordon Worley <vadixidav@gmail.com>
2019-08-27 23:55:42 -0400

    fix sobel example after revamp

--- examples/sobel/src/main.rs
+++ examples/sobel/src/main.rs
@@ -22,8 +22,8 @@ fn main() {
     let image = image.map(|&n| n.into());
     let down_filter = array![[-1.0, -2.0, -1.0], [0.0, 0.0, 0.0], [1.0, 2.0, 1.0],];
     let right_filter = down_filter.t().to_owned();
-    let down = Conv2(down_filter).forward(&image).1;
-    let right = Conv2(right_filter).forward(&image).1;
+    let down = Conv2::new(down_filter).forward(&image).1;
+    let right = Conv2::new(right_filter).forward(&image).1;
     let image = (down.map(|f| f.powi(2)) + right.map(|f| f.powi(2))).map(|f| f.sqrt());
     let image = image.map(|&n| num::clamp(n, 0.0, 255.0) as u8);
     save_gray_image(opt.output, image.view()).expect("failed to write output");
