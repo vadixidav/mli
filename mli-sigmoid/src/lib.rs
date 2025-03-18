30eb85d97d8536c506bbf18b80f63c69271c1a8a
Geordon Worley <vadixidav@gmail.com>
2025-03-18 09:01:46 -0500

    add LogisticCentered

--- mli-sigmoid/src/lib.rs
+++ mli-sigmoid/src/lib.rs
@@ -40,3 +40,35 @@ impl Backward for Logistic {
 impl Train for Logistic {
     fn train(&mut self, _: &Self::TrainDelta) {}
 }
+
+#[derive(Copy, Clone, Debug)]
+pub struct LogisticCentered;
+
+impl Forward for LogisticCentered {
+    type Input = f32;
+    type Internal = EmptyData;
+    type Output = f32;
+
+    fn forward(&self, &input: &f32) -> (EmptyData, f32) {
+        (EmptyData, 2.0 * logistic(input) - 1.0)
+    }
+}
+
+impl Backward for LogisticCentered {
+    type OutputDelta = f32;
+    type InputDelta = f32;
+    type TrainDelta = f32;
+
+    fn backward(
+        &self,
+        &input: &f32,
+        _: &EmptyData,
+        &output_delta: &f32,
+    ) -> (Self::InputDelta, Self::TrainDelta) {
+        (2.0 * logistic_derivative(input) * output_delta, 0.0)
+    }
+}
+
+impl Train for LogisticCentered {
+    fn train(&mut self, _: &Self::TrainDelta) {}
+}
